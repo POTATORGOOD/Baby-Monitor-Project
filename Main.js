77b2d6ef4b92ce54e911dc6899b4939df7681785
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2022-06-08 21:40:26 -0500

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,55 @@
+status = "";
+img = "";
+objects = [];
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function preload() {
+    alert = loadSound("mixkit-video-game-mystery-alert-234.wav");
+}
+
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+   
+    if (status != "") {
+        for (i == 0; i < objects.length; i++) {
+            if (objects[i].label != "person") {
+                alert.play();
+                document.getElementById("number_of_objects").innerHTML = "baby not found"
+                break
+            }
+        }
+    }
+    
+}
+
